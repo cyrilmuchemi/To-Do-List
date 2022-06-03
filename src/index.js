792c37a63b4c7eeb2547445da55fe316fc8ee3bd
afikanzoro <muchemimutahi@gmail.com>
2022-06-03 18:12:58 +0300

    Add functionalities

--- src/index.js
+++ src/index.js
@@ -5,36 +5,67 @@ import './style.css';
 const form = document.querySelector('.form');
 const container = document.querySelector('.listContainer');
 
-const arr = [
-  {
-    descr: 'Prepare for Coding challenge',
-    completed: true,
-    index: 1,
-  },
+class ListObject {
+  constructor(description, completed, index) {
+    this.description = description
+    this.completed = completed
+    this.index = index
+  }
+}
 
-  {
-    descr: 'Complete current project',
-    completed: true,
-    index: 2,
-  },
+const arr = [];
 
-  {
-    descr: 'Watch some anime',
-    completed: false,
-    index: 3,
-  },
-];
 
-const list = [];
-
-for (let i = 0; i < arr.length; i += 1) {
-  list[i] = document.createElement('div');
-  list[i].className = 'list';
-  list[i].innerHTML += `
+const toDo = () => {
+  const list = document.createElement('div');
+  const value = document.querySelector('.input').value;
+  list.className = 'list';
+  list.innerHTML += `
   <input type= "checkbox" class= "checkbox">
-  <span>${arr[i].descr}</span>
+  <span>${value}</span>
   <i class="fas fa-ellipsis-v"></i>
   <i class="fas fa-trash"></i>
-  `;
-  container.appendChild(list[i]);
+  <hr>
+  `
+  container.appendChild(list);
+
+  const checkbox = document.querySelectorAll('.checkbox');
+  checkbox.forEach(box => {
+    box.addEventListener('click', () => {
+      box.parentElement.classList.toggle('checkedHolder');
+      box.nextElementSibling.classList.toggle('completedList');
+      box.parentElement.lastElementChild.previousElementSibling.classList.toggle('trash-active');
+      box.parentElement.lastElementChild.previousElementSibling.previousElementSibling.classList.toggle('ellipse-disable');
+    })
+  });
+
+  const object = new ListObject(value, false, checkbox.length -1);
+  arr.push(object);
+  localStorage.setItem('list', JSON.stringify(arr));
+
+  const ellipse = document.querySelectorAll('.fa-ellipsis-v');
+  ellipse.forEach(dots => {
+    dots.addEventListener('click', () => {
+      currentTask()
+    })
+  });
+
+  function currentTask(toDo, list) {
+    const editTask = document.createElement('input');
+    editTask.type = 'text';
+    editTask.className = 'input';
+    editTask.value = '';
+
+
+  }
 }
+
+const clearField = () => {
+  document.querySelector('.input').value = ''
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  toDo()
+  clearField()
+});
