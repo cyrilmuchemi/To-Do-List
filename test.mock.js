3243cacba8c9ed28b6b3fd52495c7b7d205b6f12
unknown <sntakirutimana72@gmail.com>
2022-06-10 11:42:34 +0200

    Add test units for update and clear all tasks

--- test.mock.js
+++ test.mock.js
@@ -1,14 +1,16 @@
-const BODY = `
-    <form data-testid="form" class="form">
+/**
+ * @jest-environment jsdom
+ */
+
+document.body.innerHTML = `
+    <form class="form">
         <div>
-            <h1 data-testid="h1">Today's To Do<i class="fas fa-sync-alt"></i></h1>
+            <h1>Today's To Do<i class="fas fa-sync-alt"></i></h1>
             <hr>
-            <input data-testid="new-input" class="input" type="text" placeholder="Add to your list...">
+            <input class="input" type="text" placeholder="Add to your list...">
         </div>
-        <div data-testid="records" class="listContainer"></div>
+        <div class="listContainer"></div>
     </form>
 
     <script defer src="src/index.js"></script>
 `;
-
-document.body.innerHTML = BODY;
